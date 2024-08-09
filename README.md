59afda9cfb3f09853b626626526b6e93a8791455
AnhMTV <maithachvietanh@gmail.com>
2024-08-09 15:48:52 +0700

    Update documentation

--- README.md
+++ README.md
@@ -1,20 +1,253 @@
-# Game SDK
+# Overview
 
-This is a SDK for integrating game into Playnation.
+This is an SDK for integrating games into Playnation.
 
-The first SDK version is following the concept from https://github.com/wallacy-io/game-sdk-example
+The first SDK version follows the concept from [game-sdk-example](https://github.com/wallacy-io/game-sdk-example).
 
-## Integration
+Online Demo: [https://playnation-game-sdk.netlify.app/](https://playnation-game-sdk.netlify.app/)
 
-Please see in the iframe example in the `example` folder.
+## Concepts
+- Connect game on the iframe with `postMessage` and window listening.
+- The game will interact with the game server through the SDK and parent window. 
 
-### Error handling
+## Play the game
+What is implemented in the [game.ts](examples%2Fiframe-implementation%2Fsrc%2Fgame.ts) or [game-farming.ts](examples%2Fiframe-implementation%2Fsrc%2Fgame-farming.ts):
 
-Coming soon
+Init flow: Get basic information
+- `init()`
+- `getSDKVersion()`
+- `getPlayer()`
+- `getTournament()`
 
-## Build
+Play casual game flow
+- `play()` start the game
+- `trackScore()` on playing
+- `signResult()` when game is over
+- `getPlayer()` to retrieve player data
+
+Play farming game
+- `play()` start the game
+- `signPayload()` sign to get the game state with key
+- `updateState()` update signed payload every time the game state changes
+
+---
+
+# How to use
+
+## Run the example
+clone this repository and run the example:
+```sh
+git clone
+cd ./examples/iframe-implementation
+yarn && yarn dev
+```
+
+## Quick start
+Include the SDK in your HTML file:
+```html
+<script src="https://unpkg.com/@playnation/game-sdk@latest/browser/sdk.js"></script>
+```
+
+Create a JavaScript file (e.g., main.js) with something like this example below:
+```js
+document.addEventListener('DOMContentLoaded', async () => {
+  const sdk = window.PlaynationGameSDK;
+  
+  // Run on init
+  const initParams = { clientId: 'your-client-id' };
+  const initResponse = await sdk.init(initParams);
+  console.log('SDK Initialized:', initResponse);
+
+  // Get SDK version
+  const version = sdk.getVersion();
+  console.log('SDK Version:', version);
+
+  // Get player information
+  let player = await sdk.getPlayer();
+  console.log('Player Info:', player);
+
+  // Get tournament information
+  const tournament = await sdk.getTournament();
+  console.log('Tournament Info:', tournament);
+
+  // Play the game
+  async function playGame() {
+    // Start the game play
+    const playResponse = await sdk.play();
+    console.log('Play Response:', playResponse);
+
+    // Track score
+    await sdk.trackScore(playResponse.token, 100);
+    console.log('Score tracked');
+
+    // Sign result
+    const signedResult = await sdk.signResult(playResponse.token, 'game-token', 100);
+    console.log('Signed Result:', signedResult);
+    
+    // Get player information
+    player = await sdk.getPlayer();
+  }
+});
+```
+
+## Installation
+
+### Install with npm
+npm:
+```sh
+npm install @playnation/sdk
+```
+
+or with yarn: 
+```sh
+yarn add @playnation/sdk
+```
+
+### Get from CDN
+
+Use latest version:
+```html
+<script src="https://unpkg.com/@playnation/game-sdk@latest/browser/sdk.js"></script>
+```
+
+Or specify version:
+```html
+<script src="https://unpkg.com/@playnation/game-sdk@1.1.0/browser/sdk.js"></script>
+```
+
+## Implementation
+### Methods
+
+#### `init(params: SDKInitParams): Promise<{ currentTimestamp: string }>`
+Initializes the SDK with the given parameters.
+
+- **params**: Initialization parameters.
+- **Returns**: A promise that resolves with the current timestamp.
+
+#### `getVersion(): string`
+Gets the SDK version.
+
+- **Returns**: The SDK version.
+
+#### `getPlayer(): Promise<Player>`
+Gets the player information.
+
+- **Returns**: A promise that resolves with the player information.
+- **player**: Player information will include the following fields:
+  - **id**: The player ID.
+  - **name**: The player name.
+  - **totalScore**: The player's total score.
+  - **highScore**: The player's highest score.
+  - **balanceNPS**: The player's NPS.
+  - **energy**: The player's energy.
+  - **gameEnergy**: Energy cost each play time.
+  - **pointConversionRate**: The point conversion rate.
+
+#### `getTournament(): Promise<Tournament | undefined>`
+Gets the tournament information.
+
+- **Returns**: A promise that resolves with the tournament information or undefined if no tournament is found.
+
+#### `play(): Promise<PlayResponse>`
+Starts the game play.
+
+- **Returns**: A promise that resolves with the play response.
+
+#### `trackScore(gamePlayId: string, score: number): Promise<void>`
+Tracks the score for a given game play.
+
+- **gamePlayId**: The game play ID.
+- **score**: The score to track.
+- **Returns**: A promise that resolves when the score is tracked.
+
+#### `signResult(gamePlayId: string, gameToken: string, score: number): Promise<string>`
+Signs the result of a game play.
+
+- **gamePlayId**: The game play ID.
+- **gameToken**: The game token.
+- **score**: The score to sign.
+- **Returns**: A promise that resolves with the signed result.
+
+#### `updateState(payload: UpdateStatePayload): Promise<boolean>`
+Updates the state of the game.
+
+- **payload**: The state payload to update.
+- **Returns**: A promise that resolves with a boolean indicating success.
+
+#### `signPayload(payload: any, key: string): Promise<any>`
+Signs a payload with a given key.
+
+- **payload**: The payload to sign.
+- **key**: The key to sign the payload with.
+- **Returns**: A promise that resolves with the signed payload.
+
+## Error handling
+
+### Error Codes
+
+
+Enum representing various error codes that can be returned by the SDK.
+
+- **`SYSTEM_ERROR`**: `-1`
+  - Description: Something went wrong.
+  
+- **`INVALID_REQUEST`**: `10`
+  - Description: The request is invalid.
+  
+- **`INVALID_SCORE`**: `120`
+  - Description: The score was not accepted (cheat detected).
+  
+- **`USER_REJECT`**: `130`
+  - Description: The user rejected the transaction (buy tickets or items).
+  
+- **`NOT_ENOUGH_ENERGY`**: `150`
+  - Description: Not enough energy to play the game.
+  
+- **`TOUR_NOT_AVAILABLE`**: `100`
+  - Description: The tournament has ended or is disabled.
+  
+- **`NOT_ENOUGH_NPS`**: `110`
+  - Description: Not enough NPS to buy tickets or items.
+  
+- **`NOT_ENOUGH_TICKET`**: `140`
+  - Description: Not enough tickets to play the game.
+
+---
+# Development
+
+## Develop with example
+
+### Develop game iframe:
+  
+```sh
+cd ./examples/iframe-implementation
+yarn && yarn dev
+```
+
+You also develop your game from the example by modifying the `index.html` file.
+Change the `src` attribute of the iframe to your game URL.
+
+```html
+<button class="game-button" data-url="[your-game-url]" type="button">Casual Game</button>
+<button class="game-button" data-url="[your-game-url]" type="button">Farming Game</button>
+```
+
+### Build example:
 
 ```sh
-$ yarn
-$ yarn build
+cd ./examples/iframe-implementation
+yarn && yarn build
+```
+
+## Develop SDK:
+Example will use the SDK from the `src` folder in the root directory instead of from `node_modules`.
+```sh
+cd ./examples/iframe-implementation
+yarn && yarn dev:source
+```
+
+## Build SDK
+
+```sh
+yarn && yarn build
 ```
--- src/websdk.ts
+++ src/websdk.ts
@@ -7,36 +7,83 @@ import {
   SDKInitParams,
   Tournament, UpdateStatePayload, UseInGameItemResponse
 } from "./types";
-import { signPayload } from "./utils";
+import {signPayload} from "./utils";
 
 const SDK_VERSION = '1.1.0'; // Update this version when you update the SDK
 
+/**
+ * IframeSDK class implements the GameSDK interface and provides methods to interact with the game through an iframe.
+ */
 export class IframeSDK implements GameSDK {
   private requestId = 0;
+
+  /**
+   * Initializes the SDK with the given parameters.
+   * @param {SDKInitParams} params - Initialization parameters.
+   * @returns {Promise<{ currentTimestamp: string }>} - A promise that resolves with the current timestamp.
+   */
   async init(params: SDKInitParams) {
-    return await this.dispatch<{ currentTimestamp: string }>('INIT', params);
+    return await this.dispatch<{ currentTimestamp: string }>('INIT', {
+      ...params,
+      version: SDK_VERSION
+    });
   }
+
+  /**
+   * Gets the SDK version.
+   * @returns {string} - The SDK version.
+   */
+  getVersion() {
+    return SDK_VERSION;
+  }
+
+  /**
+   * Gets the player information.
+   * @returns {Promise<Player>} - A promise that resolves with the player information.
+   */
   async getPlayer() {
     return await this.dispatch<Player>('GET_PLAYER');
   }
+
+  /**
+   * Gets the tournament information.
+   * @returns {Promise<Tournament | undefined>} - A promise that resolves with the tournament information or undefined if no tournament is found.
+   */
   async getTournament() {
     const t = await this.dispatch<Tournament>('GET_TOURNAMENT');
     if (!t.id) return;
 
     return t;
   }
-  async buyTickets() {
-    return await this.dispatch<{ balance: number; tickets: number }>('BUY_TICKET');
-  }
+
+  /**
+   * Starts the game play.
+   * @returns {Promise<PlayResponse>} - A promise that resolves with the play response.
+   */
   async play() {
     return await this.dispatch<PlayResponse>('PLAY');
   }
+
+  /**
+   * Tracks the score for a given game play.
+   * @param {string} gamePlayId - The game play ID.
+   * @param {number} score - The score to track.
+   * @returns {Promise<void>} - A promise that resolves when the score is tracked.
+   */
   async trackScore(gamePlayId: string, score: number) {
     return await this.dispatch<void>('TRACK_SCORE', {
       gamePlayId,
       score
     });
   }
+
+  /**
+   * Signs the result of a game play.
+   * @param {string} gamePlayId - The game play ID.
+   * @param {string} gameToken - The game token.
+   * @param {number} score - The score to sign.
+   * @returns {Promise<string>} - A promise that resolves with the signed result.
+   */
   async signResult(gamePlayId: string, gameToken: string, score: number) {
     return await this.dispatch<string>('SIGN_RESULT', {
       gamePlayId,
@@ -44,46 +91,83 @@ export class IframeSDK implements GameSDK {
       score
     });
   }
+
+  /**
+   * Updates the state of the game.
+   * @param {UpdateStatePayload} payload - The state payload to update.
+   * @returns {Promise<boolean>} - A promise that resolves with a boolean indicating success.
+   */
   async updateState(payload: UpdateStatePayload): Promise<boolean> {
     return await this.dispatch<boolean>('UPDATE_STATE', payload);
   }
+
+    /**
+   * Signs a payload with a given key.
+   * @param {any} payload - The payload to sign.
+   * @param {string} key - The key to sign the payload with.
+   * @returns {Promise<any>} - A promise that resolves with the signed payload.
+   */
+  async signPayload(payload: any, key: string) {
+    return await signPayload(payload, key);
+  }
+  
+  /*=== Bellow methods will be implemented in the near future ===*/
+
+  /**
+   * Shows the leaderboard.
+   * @returns {Promise<void>} - A promise that resolves when the leaderboard is shown.
+   */
   async showLeaderboard() {
     return await this.dispatch<void>('SHOW_LEADERBOARD');
   }
-  async showShop() {
-    return await this.dispatch<void>('SHOW_SHOP');
-  }
-  async getLeaderboard(limit: number, after: string, before: string) {
-    return await this.dispatch<GetLeaderboardResponse>('GET_LEADERBOARD', { limit, after, before });
-  }
-  async getInGameItems() {
-    return await this.dispatch<{ items: InGameItem[] }>('GET_INGAME_ITEMS');
-  }
-  async buyInGameItem(itemId:string, gameplayId?:string) {
-    return await this.dispatch<BuyInGameItemResponse>('BUY_INGAME_ITEM', { itemId, gameplayId });
-  }
-  async exit(confirm = true) {
-    return await this.dispatch<void>('EXIT', confirm);
-  }
-  async exitToListGames(confirm = true) {
-    return await this.dispatch<void>('EXIT_TO_LIST_GAMES', confirm);
-  }
-  getVersion() {
-    return SDK_VERSION;
-  }
+
+  /**
+   * Triggers haptic feedback.
+   * @param {HapticFeedbackType} type - The type of haptic feedback to trigger.
+   * @returns {Promise<void>} - A promise that resolves when the haptic feedback is triggered.
+ */
   async triggerHapticFeedback(type: HapticFeedbackType) {
     await this.dispatch('TRIGGER_HAPTIC_FEEDBACK', type);
   }
-  async useInGameItem(itemId:string, gameplayId?:string) {
-    return await this.dispatch<UseInGameItemResponse>('USE_INGAME_ITEM', { itemId, gameplayId });
-  }
-  async signPayload (payload: any, key: string) {
-    return await signPayload(payload, key);
+  
+  
+  /*=== Bellow methods will be implemented in the future ===*/
+  async buyTickets() {
+    return await this.dispatch<{ balance: number; tickets: number }>('BUY_TICKET');
   }
 
-  // Create request and listen for the response
-  private requests: Record<number, { resolve: (data: any) => any; reject: (reason: string) => any }> = {};
+  async showShop() {
+    return await this.dispatch<void>('SHOW_SHOP');
+  }
+
+  async getLeaderboard(limit: number, after: string, before: string) {
+    return await this.dispatch<GetLeaderboardResponse>('GET_LEADERBOARD', {limit, after, before});
+  }
+
+  async getInGameItems() {
+    return await this.dispatch<{ items: InGameItem[] }>('GET_INGAME_ITEMS');
+  }
+
+  async buyInGameItem(itemId: string, gameplayId?: string) {
+    return await this.dispatch<BuyInGameItemResponse>('BUY_INGAME_ITEM', {itemId, gameplayId});
+  }
+
+  async exit(confirm = true) {
+    return await this.dispatch<void>('EXIT', confirm);
+  }
+
+  async exitToListGames(confirm = true) {
+    return await this.dispatch<void>('EXIT_TO_LIST_GAMES', confirm);
+  }
+
+  async useInGameItem(itemId: string, gameplayId?: string) {
+    return await this.dispatch<UseInGameItemResponse>('USE_INGAME_ITEM', {itemId, gameplayId});
+  }
+
   
+  /*=== Methods to interact with Playnation parent page ===*/
+  private requests: Record<number, { resolve: (data: any) => any; reject: (reason: string) => any }> = {};
+
   async dispatch<T>(action: string, data?: any, timeout = 60): Promise<T> {
     return new Promise((resolve, reject) => {
       const requestId = this.requestId + 1;
@@ -95,20 +179,21 @@ export class IframeSDK implements GameSDK {
         }, timeout * 1000);
       }
 
-      this.requests[requestId] = { resolve, reject };
-      const msg = { source: 'game-sdk', requestId, action, data };
-      
+      this.requests[requestId] = {resolve, reject};
+      const msg = {source: 'game-sdk', requestId, action, data};
+
       window.parent.postMessage(msg, '*');
     });
   }
+
   onMessage(event: MessageEvent) {
     if (typeof event.data !== 'object') return;
 
-    const { requestId, ...payload } = event.data;
+    const {requestId, ...payload} = event.data;
     if (!this.requests[requestId]) return;
 
     // console.log('SDK.onMesasge', event.data);
-    const { resolve, reject } = this.requests[requestId]!;
+    const {resolve, reject} = this.requests[requestId]!;
     if (payload.error) {
       const err: any = new Error(payload.error);
       err.code = payload.code;
@@ -118,10 +203,10 @@ export class IframeSDK implements GameSDK {
     }
     delete this.requests[requestId];
   }
-  
+
   // Singleton
   private static _instance: IframeSDK;
-  
+
   static get instance() {
     if (!IframeSDK._instance) {
       IframeSDK._instance = new IframeSDK();
